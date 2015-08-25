92391b9631ea6d2c82b4e20cc9986e9acc15bbda
liu <hd996688@163.com>
2015-08-25 09:17:39 +0800

    Signed-off-by: liu <hd996688@163.com>

--- test/com/smartlab/oa/test/LogTest.java
+++ test/com/smartlab/oa/test/LogTest.java
@@ -11,11 +11,11 @@ public class LogTest {
 	//日志错误信息优先级
 	@Test
 	public void test() throws Exception{
-		log.debug("这是debug信息");
-		log.info("这是info信息");
-		log.warn("这是warn信息");
+		log.debug("这是debug信息"); //调试信息
+		log.info("这是info信息");  //一般信息
+		log.warn("这是warn信息"); //警告信息
 		log.error("这是error信息");  //在log4j.properties文件中设置log4j.rootLogger=error, stdout
-		log.fatal("这是fatal信息");
+		log.fatal("这是fatal信息"); //严重信息
 		
 		//log4j.logger.com.smartlab.oa=debug 这个包中的错误日志信息模式为debug
 	}
